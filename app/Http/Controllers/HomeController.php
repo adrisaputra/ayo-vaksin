ffe7168594d10b7b1985d2edeabbf436fcee017a
adri <adri.saputra.ibrahim@gmail.com>
2021-10-03 20:33:23 +0800

    New Update 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,10 +16,16 @@ class HomeController extends Controller
 	
     public function index()
     {
+        if(Auth::user()->group==1){
+            $jumlah_antrian = Antrian::where('tanggal',date('Y-m-d'))->where('status_hapus', 0)->count();
+            $sudah_divaksin = Antrian::where('tanggal',date('Y-m-d'))->where('status', 1)->where('status_hapus', 0)->count();
+            $belum_divaksin = Antrian::where('tanggal',date('Y-m-d'))->where('status', 0)->where('status_hapus', 0)->count();
+        } else {
             $jumlah_antrian = Antrian::where('tanggal',date('Y-m-d'))->where('status_hapus', 0)->where('faskes', Auth::user()->faskes)->count();
             $sudah_divaksin = Antrian::where('tanggal',date('Y-m-d'))->where('status', 1)->where('status_hapus', 0)->where('faskes', Auth::user()->faskes)->count();
             $belum_divaksin = Antrian::where('tanggal',date('Y-m-d'))->where('status', 0)->where('status_hapus', 0)->where('faskes', Auth::user()->faskes)->count();
-        
+        }
+           
         return view('admin.beranda', compact('jumlah_antrian','sudah_divaksin','belum_divaksin'));
     }
 }
